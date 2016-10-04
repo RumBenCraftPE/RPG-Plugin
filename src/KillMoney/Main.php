1273b218f532972e576915587c69c5307d60fddc
Christopher Caldwell <endercreeper4ut@gmail.com>
2016-10-04 18:03:17 -0400

    Update Main.php

--- src/KillMoney/Main.php
+++ src/KillMoney/Main.php
@@ -12,16 +12,16 @@ use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 class Main extends PluginBase implements Listener{
 	public function onLoad(){
-		$this->getLogger()->info(TextFormat::WHITE . "KillMoney has been loaded!");
+		$this->getLogger()->info(TextFormat::WHITE . "RPG has been loaded!");
 	}
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		  $this->config = (new Config($this->getDataFolder()."config.yml", Config::YAML))->getAll();
        $this->saveDefaultConfig();
-		$this->getLogger()->info(TextFormat::DARK_GREEN . "KillMoney has offically been enabled!");
+		$this->getLogger()->info(TextFormat::DARK_GREEN . "RPG has offically been enabled!");
     }
 	public function onDisable(){
-		$this->getLogger()->info(TextFormat::DARK_RED . "KillMoney has been disabled!");
+		$this->getLogger()->info(TextFormat::DARK_RED . "RPG has been disabled!");
 	}
 	
 public function addMoney($name,$money){
@@ -38,4 +38,59 @@ $player->sendMessage("You've Earned 8 Coins");
 $this->addMoney($player->getName(), $money);
 }
 }
-}
+	
+	// RPG CLASSES FLOATINGTEXT START //
+	public function onJoin(PlayerJoinEvent $event){
+		$p=$event->getPlayer();
+		// CLASSES Select Selected //
+		
+		 $config = new Config($this->getDataFolder() . "/classes.yml", Config::YAML);
+    $class="false";
+		if($config->get("{$player->getName()}") != null)
+		{
+       
+      $player->sendMessage("§7Choose the same class to Continue!");
+      $class="true";
+		}
+		else{
+			
+			$player->sendMessage("Â§7Please choose a Class!");
+			$class="test";
+		}
+		if($class=="true"){
+				 $config = new Config($this->getDataFolder() . "/class.yml", Config::YAML);
+				 //$world=$config->get("{$player}-world");
+				//$spawn = $this->getServer()->getLevelByName($world)->getSafeSpawn();
+					//$this->getServer()->getLevelByName($world)->loadChunk($spawn->getX(), $spawn->getZ());
+					//$player->teleport($spawn,0,0);
+						$p=$player->getName();
+					$cl = $config->get($p);
+               $class = $cl["class"];
+				$player->sendMessage("Your Previous class was {$class}");
+$clas=$class;
+				
+			}
+		if($class="Wizard"){
+			
+		$level->addParticle(new FloatingTextParticle(new Vector3(146.5, 72, -1862.5),"", "§l§cSelected"));
+		}
+		else{$level->addParticle(new FloatingTextParticle(new Vector3(146.5, 72, -1862.5),"", "§l§cSelect"));}
+
+	
+	if($class="Warrior"){
+			
+		$level->addParticle(new FloatingTextParticle(new Vector3(146.5, 72, -1862.5),"", "§l§cSelected"));
+		}
+		else{$level->addParticle(new FloatingTextParticle(new Vector3(146.5, 72, -1862.5),"", "§l§cSelect"));}
+		
+		
+		if($class="Archer"){
+			
+		$level->addParticle(new FloatingTextParticle(new Vector3(146.5, 72, -1862.5),"", "§l§cSelected"));
+		}
+		else{$level->addParticle(new FloatingTextParticle(new Vector3(146.5, 72, -1862.5),"", "§l§cSelect"));}
+		
+		
+		}
+	}
+
